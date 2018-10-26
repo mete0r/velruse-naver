a4a74cf56cce8ce60e67d30bef3385067d48e8f0
mete0r <mete0r@sarangbang.or.kr>
2018-10-27 00:27:30 +0900

    Back to development: 0.1.1

--- src/velruse_naver/__init__.py
+++ src/velruse_naver/__init__.py
@@ -34,7 +34,7 @@ from velruse.exceptions import ThirdPartyFailure
 from velruse.settings import ProviderSettings
 from velruse.utils import flat_url
 
-__version__ = '0.1.0'
+__version__ = '0.1.1.dev0'
 
 logger = logging.getLogger(__name__)
 
